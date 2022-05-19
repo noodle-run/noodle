5a467bf7eaa06e17b42bb9124a3ad33e22e83222
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-05-19 06:35:28 +0100

    update it again

--- apps/client/pages/index.tsx
+++ apps/client/pages/index.tsx
@@ -2,7 +2,8 @@ import { NextPage } from 'next';
 
 const Home: NextPage = () => (
   <div>
-    <h1>Noodle - Open Source Education Platform</h1>
+    <h1>Noodle - Open Source Educational Platform</h1>
+    <h1>Hello World</h1>
   </div>
 );
 
