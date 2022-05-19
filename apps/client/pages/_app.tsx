e6b689894b36bb2a04cdc4d9504d6909d74d5651
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-05-19 06:01:47 +0100

    feat(client): adds favicon and changes title

--- apps/client/pages/_app.tsx
+++ apps/client/pages/_app.tsx
@@ -4,9 +4,30 @@ import Head from 'next/head';
 const App = ({ Component, pageProps }: AppProps) => (
   <>
     <Head>
-      <title>Welcome to client!</title>
+      <title>Noodle - Open Source Educational Platform</title>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff" />
     </Head>
-    <main className="app">
+    <main>
       <Component {...pageProps} />
     </main>
   </>
