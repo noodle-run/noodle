ba00670a92c77db7c0fba0e882461add1096fdf6
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-05-19 04:55:34 +0100

    cleanup client app

--- apps/client/pages/_app.tsx
+++ apps/client/pages/_app.tsx
@@ -1,18 +1,15 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import './styles.css';
 
-function CustomApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <title>Welcome to client!</title>
-      </Head>
-      <main className="app">
-        <Component {...pageProps} />
-      </main>
-    </>
-  );
-}
+const App = ({ Component, pageProps }: AppProps) => (
+  <>
+    <Head>
+      <title>Welcome to client!</title>
+    </Head>
+    <main className="app">
+      <Component {...pageProps} />
+    </main>
+  </>
+);
 
-export default CustomApp;
+export default App;
