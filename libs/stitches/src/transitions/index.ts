9ce9fe1022ba86292efa5850907e31d7a2c5fa5d
Aleksandr Ural <aleksandrural@hotmail.com>
2022-05-27 23:08:23 +0100

    fix(stitches): fixes invalid css transition values

--- libs/stitches/src/transitions/index.ts
+++ libs/stitches/src/transitions/index.ts
@@ -1,10 +1,12 @@
 export const easing = 'cubic-bezier(0.4, 0, 0.2, 1)';
 export const timing = '150ms';
 
+const colors = `color ${easing} ${timing}, background-color ${easing} ${timing}, border-color ${easing} ${timing}, text-decoration-color ${easing} ${timing}, fill ${easing} ${timing}, stroke ${easing} ${timing}`;
+
 export const transitions = {
   all: `all ${easing} ${timing}`,
-  default: `color, background-color, border-color, text-decoration-color, fill, stroke, opacity, box-shadow, transform, filter, backdrop-filter ${easing} ${timing}`,
-  colors: `color, background-color, border-color, text-decoration-color, fill, stroke ${easing} ${timing}`,
+  default: `${colors}, opacity ${easing} ${timing}, box-shadow ${easing} ${timing}, transform ${easing} ${timing}, filter ${easing} ${timing}, backdrop-filter ${easing} ${timing}`,
+  colors,
   opacity: `opacity ${easing} ${timing}`,
   shadow: `box-shadow ${easing} ${timing}`,
   transform: `transform ${easing} ${timing}`,
