30feb9c637d2108be6a048c8642b0a5832912095
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-06-01 23:34:29 +0100

    feat(stitches): adds letter spacings documentation

---
+++ libs/stitches/src/story-components/LetterSpacings/LetterSpacings.tsx
@@ -0,0 +1,25 @@
+import { styled } from '../../stitches.config';
+import { letterSpacings } from '../../typography';
+
+const Name = styled('p', {
+  paddingTop: '$3',
+  paddingBottom: '$2',
+  opacity: 0.5,
+});
+
+const Text = styled('p', {
+  fontSize: '$2xl',
+});
+
+export const LetterSpacings = () => (
+  <div>
+    {Object.keys(letterSpacings).map((spacing) => (
+      <div key={spacing}>
+        <Name>{spacing}:</Name>
+        <Text css={{ letterSpacing: `$${spacing}` }}>
+          The quick brown fox jumped over the lazy dog.
+        </Text>
+      </div>
+    ))}
+  </div>
+);
