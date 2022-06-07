76dd2d5b64704dadf2be3035c544e4fc390b6763
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-06-07 23:13:33 +0100

    refactor(client): next public vercel url env var

--- apps/client/pages/_app.tsx
+++ apps/client/pages/_app.tsx
@@ -38,8 +38,8 @@ const App = ({ Component, pageProps }: AppProps) => (
 
 export default withTRPC<AppRouter>({
   config() {
-    const url = process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/api/trpc`
+    const url = process.env.NEXT_PUBLIC_VERCEL_URL
+      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`
       : 'http://localhost:4200/api/trpc';
 
     return {
