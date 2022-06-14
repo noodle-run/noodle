40980e60c64c9c6a8565c7a52226113c1646bf40
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-06-14 10:54:05 +0100

    feat(design-system): adds button atom

--- libs/design-system/src/atoms/Button/Button.stories.tsx
+++ libs/design-system/src/atoms/Button/Button.stories.tsx
@@ -1,18 +1,38 @@
+import { expect } from '@storybook/jest';
 import { Meta, Story } from '@storybook/react';
+import { userEvent, within } from '@storybook/testing-library';
 import { ComponentProps } from 'react';
+import { FiPlusCircle } from 'react-icons/fi';
 import { Button } from './Button';
 
 const args: ComponentProps<typeof Button> = {
   children: 'Add new',
+  icon: <FiPlusCircle />,
 };
 
 const config: Meta<typeof args> = {
   title: 'Design System / Atoms / Button',
   component: Button,
   args,
+  argTypes: {
+    onClick: {
+      action: true,
+    },
+    icon: {
+      control: false,
+    },
+    type: {
+      control: false,
+    },
+  },
 };
 
 export default config;
 
 export const Normal: Story<typeof args> = (props) => <Button {...props} />;
 Normal.storyName = 'Button';
+Normal.play = async ({ canvasElement, args: props }) => {
+  const canvas = within(canvasElement);
+  userEvent.click(canvas.getByRole('button'));
+  expect(props.onClick).toHaveBeenCalled();
+};
--- libs/design-system/src/atoms/Button/Button.tsx
+++ libs/design-system/src/atoms/Button/Button.tsx
@@ -1,14 +1,54 @@
 import { styled } from '@noodle/stitches';
 import { FC, ReactNode } from 'react';
 
-const StyledButton = styled('button', {
-  backgroundColor: '$pink10',
+const Icon = styled('span', {
+  height: '100%',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  padding: '$1',
+  backgroundColor: '$gray5',
+  borderRadius: '$full',
+  boxShadow: '$md',
+  transition: '$colors',
+});
+
+const Wrapper = styled('button', {
+  backgroundColor: '$gray3',
+  p: '$2 $4',
+  borderRadius: '$full',
+  fontSize: '$xs',
+  color: '$gray11',
+  boxShadow: '$lg',
+  outline: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  gap: '$2',
+  transition: '$colors',
+  '&:hover': {
+    backgroundColor: '$gray4',
+    '& span': {
+      backgroundColor: '$gray6',
+    },
+  },
 });
 
 type ButtonProps = {
   readonly children: ReactNode;
+  readonly icon?: ReactNode;
+  readonly onClick?: () => void;
+  readonly type?: 'button' | 'reset' | 'submit';
 };
 
-export const Button: FC<ButtonProps> = ({ children }) => (
-  <StyledButton>{children}</StyledButton>
+export const Button: FC<ButtonProps> = ({
+  children,
+  onClick,
+  icon,
+  type = 'button',
+}) => (
+  <Wrapper type={type ?? 'button'} onClick={onClick}>
+    {children}
+    <Icon>{icon}</Icon>
+  </Wrapper>
 );
