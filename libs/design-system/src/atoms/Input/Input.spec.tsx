65f11545f8e7acd32be63c90be08b7184c340ca7
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-06-18 16:01:49 +0100

    refactor(design-system): allow input atom to be more customisable

--- libs/design-system/src/atoms/Input/Input.spec.tsx
+++ libs/design-system/src/atoms/Input/Input.spec.tsx
@@ -1,23 +1,29 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { ComponentProps } from 'react';
 import { Input } from './Input';
+
+const args: ComponentProps<typeof Input> = {
+  type: 'text',
+  placeholder: 'Search...',
+};
 
 describe('Input field atom', () => {
   it('renders an input field', () => {
-    render(<Input />);
+    render(<Input {...args} />);
 
     expect(screen.getByRole('textbox')).toBeInTheDocument();
   });
 
   it('renders the input field with a value', () => {
-    render(<Input value="initial value" />);
+    render(<Input {...args} value="initial value" />);
 
     expect(screen.getByRole('textbox')).toHaveValue('initial value');
   });
 
   it('emits the input onchange when typed into it', async () => {
     const onChange = jest.fn();
-    render(<Input onChange={onChange} />);
+    render(<Input {...args} onChange={onChange} />);
 
     await userEvent.type(screen.getByRole('textbox'), 'new value');
 
@@ -26,7 +32,7 @@ describe('Input field atom', () => {
   });
 
   it('renders the input field with an icon', () => {
-    render(<Input icon={<p>look at me, im the icon now</p>} />);
+    render(<Input {...args} icon={<p>look at me, im the icon now</p>} />);
 
     expect(screen.getByRole('presentation')).toBeInTheDocument();
   });
