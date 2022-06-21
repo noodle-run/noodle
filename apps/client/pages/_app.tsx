5727e7ca3e69f8f29233f1210d5814887f7c5f9f
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-06-21 03:02:12 +0100

    fix: attempt to fix deployment api url

--- apps/client/pages/_app.tsx
+++ apps/client/pages/_app.tsx
@@ -38,12 +38,8 @@ const App = ({ Component, pageProps }: AppProps) => (
 
 export default withTRPC<AppRouter>({
   config() {
-    const url = process.env.NEXT_PUBLIC_VERCEL_URL
-      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`
-      : 'http://localhost:4200/api/trpc';
-
     return {
-      url,
+      url: '/api/trpc',
     };
   },
   ssr: false,
