14aa3165992187a46c3b8222281a3617a38a04e4
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-07-02 11:55:38 +0100

    feat(design-system): adds profile picture atom

--- libs/design-system/src/atoms/index.ts
+++ libs/design-system/src/atoms/index.ts
@@ -3,6 +3,7 @@ export * from './Button';
 export * from './Input';
 export * from './Label';
 export * from './Link';
+export * from './Profile';
 export * from './ProgressBar';
 export * from './Star';
 export * from './Tag';
