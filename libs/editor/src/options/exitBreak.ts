b963fe01ce585b3747c12bdd13b1855c849516e2
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-08-25 18:40:16 +0200

    feat(editor): adds common behavior plugins

---
+++ libs/editor/src/options/exitBreak.ts
@@ -0,0 +1,23 @@
+import { ExitBreakPlugin, KEYS_HEADING, PlatePlugin } from '@udecode/plate';
+
+export const exitBreakOptions: Partial<PlatePlugin<ExitBreakPlugin>> = {
+  options: {
+    rules: [
+      {
+        hotkey: 'mod+enter',
+      },
+      {
+        hotkey: 'mod+shift+enter',
+        before: true,
+      },
+      {
+        hotkey: 'enter',
+        query: {
+          start: true,
+          end: true,
+          allow: KEYS_HEADING,
+        },
+      },
+    ],
+  },
+};
