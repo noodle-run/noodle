519b61a83d94c6219c63f04dddd5b7192c479f52
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-08-25 18:24:24 +0200

    feat(ui): adds placeholder support for h1 and p tags

--- libs/ui/src/editor/Heading/Heading.tsx
+++ libs/ui/src/editor/Heading/Heading.tsx
@@ -1,5 +1,7 @@
+import { isCollapsed, isElementEmpty, useEditorState } from '@udecode/plate';
 import { cva, VariantProps } from 'class-variance-authority';
 import { FC } from 'react';
+import { useFocused } from 'slate-react';
 import { EditorRenderElementProps } from '../../utils/EditorComponentProps';
 
 const styles = cva(['font-extrabold', 'dark:text-white', 'text-black'], {
@@ -19,6 +21,15 @@ const styles = cva(['font-extrabold', 'dark:text-white', 'text-black'], {
       h5: ['text-lg', 'opacity-90'],
       h6: ['text-base', 'opacity-70'],
     },
+    placeholder: {
+      untitled: [
+        "before:content-['Untitled']",
+        'before:block',
+        'before:cursor-text',
+        'before:absolute',
+        'before:opacity-25',
+      ],
+    },
   },
   defaultVariants: {
     variant: 'h1',
@@ -30,12 +41,30 @@ type HeadingProps = VariantProps<typeof styles> & EditorRenderElementProps;
 export const Heading: FC<HeadingProps> = ({
   attributes,
   children,
+  element,
   variant,
 }) => {
   const Component = variant || 'h1';
 
+  const focused = useFocused();
+  const editor = useEditorState();
+
+  const isEmptyBlock = element && isElementEmpty(editor, element);
+
+  const placeholderEnabled =
+    Component === 'h1' &&
+    isEmptyBlock &&
+    isCollapsed(editor.selection) &&
+    focused;
+
   return (
-    <Component {...attributes} className={styles({ variant })}>
+    <Component
+      {...attributes}
+      className={styles({
+        variant,
+        placeholder: placeholderEnabled ? 'untitled' : undefined,
+      })}
+    >
       {children}
     </Component>
   );
