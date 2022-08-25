b57cee4c726e8a03022026a3f218468b1eaeae5e
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-08-25 18:24:24 +0200

    feat(ui): adds placeholder support for h1 and p tags

--- libs/ui/src/editor/Paragraph/Paragraph.tsx
+++ libs/ui/src/editor/Paragraph/Paragraph.tsx
@@ -1,11 +1,33 @@
+import { isCollapsed, isElementEmpty, useEditorState } from '@udecode/plate';
 import { FC } from 'react';
+import { useFocused, useSelected } from 'slate-react';
+import { twMerge } from 'tailwind-merge';
 import { EditorRenderElementProps } from '../../utils/EditorComponentProps';
 
 export const Paragraph: FC<EditorRenderElementProps> = ({
   children,
   attributes,
-}) => (
-  <p className="py-3 dark:text-zinc-300 text-zinc-700" {...attributes}>
-    {children}
-  </p>
-);
+  element,
+}) => {
+  const focused = useFocused();
+  const selected = useSelected();
+  const editor = useEditorState();
+
+  const isEmptyBlock = element && isElementEmpty(editor, element);
+
+  const placeholderEnabled =
+    isEmptyBlock && isCollapsed(editor.selection) && focused && selected;
+
+  return (
+    <p
+      className={twMerge(
+        'py-3 dark:text-zinc-300 text-zinc-700',
+        placeholderEnabled &&
+          "before:content-['Type_something...'] before:block before:cursor-text before:absolute before:opacity-25",
+      )}
+      {...attributes}
+    >
+      {children}
+    </p>
+  );
+};
