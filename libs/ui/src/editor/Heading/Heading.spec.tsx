d9f87a398297881b6f1700f6ceb97dbfaff9f122
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-08-29 12:50:51 +0200

    fix(ui): fixes broken editor components tests

--- libs/ui/src/editor/Heading/Heading.spec.tsx
+++ libs/ui/src/editor/Heading/Heading.spec.tsx
@@ -1,9 +1,14 @@
 import { render, screen } from '@testing-library/react';
+import { Plate } from '@udecode/plate-core';
 import { Heading } from './Heading';
 
 describe('Heading component', () => {
   it('should render children', () => {
-    render(<Heading>Heading</Heading>);
+    render(
+      <Plate>
+        <Heading>Heading</Heading>
+      </Plate>,
+    );
 
     expect(screen.getByText(/heading/i)).toBeInTheDocument();
   });
--- libs/ui/src/editor/Paragraph/Paragraph.spec.tsx
+++ libs/ui/src/editor/Paragraph/Paragraph.spec.tsx
@@ -1,9 +1,14 @@
 import { render, screen } from '@testing-library/react';
+import { Plate } from '@udecode/plate-core';
 import { Paragraph } from './Paragraph';
 
 describe('Paragraph element', () => {
   it('should render children', () => {
-    render(<Paragraph>Paragraph</Paragraph>);
+    render(
+      <Plate>
+        <Paragraph>Paragraph</Paragraph>
+      </Plate>,
+    );
 
     expect(screen.getByText(/paragraph/i)).toBeInTheDocument();
   });
