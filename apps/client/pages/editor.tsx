0c75c79e627f0c9c5ce86ffb88b3095059a5fb9d
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-09-01 00:31:26 +0200

    feat: more responsive editor

--- apps/client/pages/editor.tsx
+++ apps/client/pages/editor.tsx
@@ -7,7 +7,7 @@ const EditorComponent = dynamic<unknown>(() => import('@noodle/editor'), {
 });
 
 const Editor: NextPage = () => (
-  <main className="max-w-2xl py-8 mx-auto">
+  <main className="max-w-2xl px-6 py-6 mx-auto md:py-8 md:px-0">
     <Suspense fallback={<div>Loading...</div>}>
       <EditorComponent />
     </Suspense>
--- libs/editor/src/components/Heading/Heading.tsx
+++ libs/editor/src/components/Heading/Heading.tsx
@@ -7,18 +7,19 @@ const styles = cva(['font-extrabold', 'dark:text-white', 'text-black'], {
   variants: {
     variant: {
       h1: [
-        'text-4xl',
+        'text-3xl',
+        'md:text-4xl',
         'border-b-2',
         'dark:border-zinc-800',
         'border-zinc-100',
         'pb-3',
         'mb-3',
       ],
-      h2: ['text-3xl', 'pt-3'],
-      h3: ['text-2xl', 'pt-2'],
-      h4: ['text-xl', 'pt-1'],
-      h5: ['text-lg', 'opacity-90'],
-      h6: ['text-base', 'opacity-70'],
+      h2: ['text-2xl', 'md:text-3xl', 'pt-5'],
+      h3: ['text-xl', 'md:text-2xl', 'pt-4'],
+      h4: ['text-lg', 'md:text-xl', 'pt-3'],
+      h5: ['text-base', 'md:text-lg', 'opacity-90', 'pt-2'],
+      h6: ['text-base', 'opacity-70', 'pt-1'],
     },
     placeholder: {
       untitled: [
