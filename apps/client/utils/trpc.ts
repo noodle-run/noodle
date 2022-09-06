67ca5577d511f75eeb4060820de0c29e25566477
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-09-06 14:31:00 +0200

    chore: updates trpc to latest version

--- apps/client/utils/trpc.ts
+++ apps/client/utils/trpc.ts
@@ -1,8 +1,8 @@
 import type { AppRouter } from '@noodle/server';
-import { setupTRPC } from '@trpc/next';
+import { createTRPCNext } from '@trpc/next';
 import superjson from 'superjson';
 
-export const trpc = setupTRPC<AppRouter>({
+export const trpc = createTRPCNext<AppRouter>({
   config() {
     return {
       url: '/api/trpc',
