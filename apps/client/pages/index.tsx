0d84e5a5a34d30a9440b7467c199f71cad436dca
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-09-09 01:53:04 +0200

    feat: creates dashboard page

--- apps/client/pages/index.tsx
+++ apps/client/pages/index.tsx
@@ -1,25 +1,6 @@
 import { Landing } from '@noodle/ui';
 import { NextPage } from 'next';
-import { useSession } from 'next-auth/react';
-import ClimbingBoxLoader from 'react-spinners/ClimbingBoxLoader';
-import { Greeting } from '../components/Greeting';
 
-const Home: NextPage = () => {
-  const { data: session, status } = useSession();
-
-  if (status === 'loading') {
-    return (
-      <div className="flex items-center justify-center h-screen w-scree">
-        <ClimbingBoxLoader color="#fa617b" />
-      </div>
-    );
-  }
-
-  if (session && session.user) {
-    return <Greeting />;
-  }
-
-  return <Landing />;
-};
+const Home: NextPage = () => <Landing />;
 
 export default Home;
