f267cf0d645a02598299e8fa08e160f87728e6f1
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-09-14 23:19:14 +0100

    chore: updates packages & adds font import

--- apps/client/pages/_app.tsx
+++ apps/client/pages/_app.tsx
@@ -1,3 +1,6 @@
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/600.css';
+import '@fontsource/inter/900.css';
 import { Session } from 'next-auth';
 import { SessionProvider } from 'next-auth/react';
 import { DefaultSeo } from 'next-seo';
