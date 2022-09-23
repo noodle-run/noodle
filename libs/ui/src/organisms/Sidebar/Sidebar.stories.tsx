71e0a80aae0cd48e7bd687ccaa745bd7a5a08aa0
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-09-23 23:47:49 +0100

    refactor(ui): moves sidebar to it's own component

---
+++ libs/ui/src/organisms/Sidebar/Sidebar.stories.tsx
@@ -0,0 +1,32 @@
+import { Meta, Story } from '@storybook/react';
+import { ComponentProps } from 'react';
+import { FiActivity } from 'react-icons/fi';
+import { Sidebar } from './Sidebar';
+
+const args: ComponentProps<typeof Sidebar> = {
+  user: {
+    name: 'Ahmed Elsakaan',
+    course: 'Computer Science',
+    avatar: 'https://avatars.githubusercontent.com/u/20271968?v=4',
+  },
+  links: [
+    {
+      label: "Today's Activity",
+      href: '/dashboard',
+      icon: <FiActivity />,
+    },
+  ],
+};
+
+const config: Meta<typeof args> = {
+  title: 'Organisms / Sidebar',
+  component: Sidebar,
+  args,
+};
+
+export default config;
+
+const Template: Story<typeof args> = (props) => <Sidebar {...props} />;
+
+export const Default = Template.bind({});
+Default.storyName = 'Sidebar';
---
+++ libs/ui/src/organisms/Sidebar/Sidebar.tsx
@@ -0,0 +1,65 @@
+import Link from 'next/link';
+import { ComponentProps, FC, useState } from 'react';
+import AnimateHeight from 'react-animate-height';
+import { FiMenu, FiX } from 'react-icons/fi';
+import { useMediaQuery } from 'usehooks-ts';
+import { Brand } from '../../atoms/Brand';
+import { SidebarLink } from '../../molecules/SidebarLink';
+import { UserCard } from '../../molecules/UserCard';
+
+type SidebarProps = {
+  user: ComponentProps<typeof UserCard>;
+  links: ComponentProps<typeof SidebarLink>[];
+};
+
+export const Sidebar: FC<SidebarProps> = ({ user, links }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const isDesktop = useMediaQuery('(min-width: 1024px)');
+
+  // eslint-disable-next-line no-nested-ternary
+  const menuHeight = isDesktop ? 'auto' : menuOpen ? 'auto' : 0;
+  return (
+    <nav className="flex flex-col justify-between w-full p-3 lg:w-72 lg:p-6">
+      <div>
+        <div className="flex items-center justify-between">
+          <Link href="/">
+            <a className="flex items-center gap-3 px-0 font-extrabold lg:px-3">
+              <Brand size={36} />
+              Noodle
+            </a>
+          </Link>
+          <div className="flex items-center">
+            {!isDesktop && <UserCard {...user} />}
+            <button
+              data-testid="menu-button"
+              type="button"
+              className="inline-block lg:hidden"
+              onClick={() => setMenuOpen((prev) => !prev)}
+            >
+              {menuOpen ? (
+                <FiX title="Close menu" size={24} />
+              ) : (
+                <FiMenu title="Open menu" size={24} />
+              )}
+            </button>
+          </div>
+        </div>
+        <AnimateHeight
+          data-testid={`menu-${menuHeight}`}
+          duration={150}
+          height={menuHeight}
+        >
+          <ul className="flex-1 pt-3 lg:pt-6">
+            {links.map((link) => (
+              <li key={link.href}>
+                <SidebarLink {...link} />
+              </li>
+            ))}
+          </ul>
+        </AnimateHeight>
+      </div>
+      {isDesktop && <UserCard {...user} />}
+    </nav>
+  );
+};
