4fdc53131396581e5367e718796a86b503632ee9
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-09-24 12:45:59 +0100

    feat(ui): adds Greeting molecule

--- libs/server/src/routers/index.ts
+++ libs/server/src/routers/index.ts
@@ -1,6 +1,7 @@
 import { t } from '../utils/trpc';
+import { quotesRouter } from './quotes.routes';
 import { userRouter } from './user.router';
 
-export const appRouter = t.mergeRouters(userRouter);
+export const appRouter = t.mergeRouters(userRouter, quotesRouter);
 
 export type AppRouter = typeof appRouter;
