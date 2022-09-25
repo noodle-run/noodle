4813e46aa4b2250ae071e6b7166ca4431e9e1b76
mabdulker <zjac264@live.rhul.ac.uk>
2022-09-25 10:47:11 +0100

    feat(ui): adds NotebookItem molecule

---
+++ libs/ui/src/molecules/NotebookItem/NotebookItem.spec.tsx
@@ -0,0 +1,35 @@
+import { render, screen } from '@testing-library/react';
+import { NotebookItem } from './NotebookItem';
+
+describe('NotebookItem molecule', () => {
+  it('renders the label and emoji', () => {
+    render(
+      <NotebookItem
+        emoji="🤖"
+        title="AI"
+        lastEdited="2 days ago"
+        label={{ name: 'Artificial Intelligence', color: 'blue' }}
+        href="/notebooks/ai"
+        defaultStarred={false}
+      />,
+    );
+
+    expect(screen.getByText('AI')).toBeInTheDocument();
+    expect(screen.getByText('🤖')).toBeInTheDocument();
+  });
+
+  it('renders correct lastEdited time', () => {
+    render(
+      <NotebookItem
+        emoji="🤖"
+        title="AI"
+        lastEdited="2 days ago"
+        label={{ name: 'Artificial Intelligence', color: 'blue' }}
+        href="/notebooks/ai"
+        defaultStarred={false}
+      />,
+    );
+
+    expect(screen.getByText('2 days ago')).toBeInTheDocument();
+  });
+});
