019815fd66c6e101d1795614b93d4cea64ae8cf0
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-09-25 03:58:55 +0100

    feat(ui): adds module card molecule

---
+++ libs/ui/src/molecules/ModuleCard/ModuleCard.spec.tsx
@@ -0,0 +1,59 @@
+import { render, screen } from '@testing-library/react';
+import { ComponentProps } from 'react';
+import { ModuleCard } from './ModuleCard';
+
+const args: ComponentProps<typeof ModuleCard> = {
+  href: '/modules/1',
+  name: 'Algorithms and Complexity',
+  code: 'CS2860',
+  tasks: 3,
+  color: 'red',
+  progress: 45,
+  icon: '🧠',
+};
+
+describe('Module Card Molecule', () => {
+  it('should render link with correct href', () => {
+    render(<ModuleCard {...args} />);
+
+    expect(screen.getByRole('link')).toHaveAttribute('href', args.href);
+  });
+
+  it('should render icon', () => {
+    render(<ModuleCard {...args} />);
+
+    expect(screen.getByText(args.icon as string)).toBeInTheDocument();
+  });
+
+  it('should render name', () => {
+    render(<ModuleCard {...args} />);
+
+    expect(screen.getByText(args.name)).toBeInTheDocument();
+  });
+
+  it('should render code', () => {
+    render(<ModuleCard {...args} />);
+
+    expect(screen.getByText(args.code)).toBeInTheDocument();
+  });
+
+  it('should render task remaining', () => {
+    render(<ModuleCard {...args} tasks={1} />);
+
+    expect(screen.getByText('1 task remaining')).toBeInTheDocument();
+  });
+
+  it('should render tasks remaining', () => {
+    render(<ModuleCard {...args} />);
+
+    expect(
+      screen.getByText(`${args.tasks} tasks remaining`),
+    ).toBeInTheDocument();
+  });
+
+  it('should render with correct color progress bar', () => {
+    render(<ModuleCard {...args} />);
+
+    expect(screen.getByRole('presentation')).toHaveClass('bg-red-400');
+  });
+});
