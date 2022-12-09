397410ed214a7fcc7505122e7a8a14663465062a
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-12-09 09:43:59 +0000

    fix: missing inter font from storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,9 +1,6 @@
-import '@fontsource/inter/400.css';
-import '@fontsource/inter/600.css';
-import '@fontsource/inter/900.css';
 import { DocsContainer, DocsContainerProps } from '@storybook/addon-docs';
 import { ThemeVars } from '@storybook/theming';
-import React, { ReactNode } from 'react';
+import { ReactNode } from 'react';
 import { useDarkMode } from 'storybook-dark-mode';
 import './globals.css';
 import { dark, light } from './theme';
