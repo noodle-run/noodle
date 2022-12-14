8ec65433d335a863a3712b83c87ea4e0b999605f
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-12-14 15:35:22 +0000

    fix(client): maybe that will work

--- apps/client/pages/_document.tsx
+++ apps/client/pages/_document.tsx
@@ -3,9 +3,9 @@ import NextDocument, { Head, Html, Main, NextScript } from 'next/document';
 export default class Document extends NextDocument {
   render() {
     return (
-      <Html lang="en" className="dark:bg-zinc-900 dark:text-white">
+      <Html lang="en">
         <Head />
-        <body>
+        <body className="dark:bg-zinc-900 dark:text-white">
           <Main />
           <NextScript />
         </body>
