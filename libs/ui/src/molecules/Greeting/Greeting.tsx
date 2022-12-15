4bad1873408e9c3b8b9b4e417534c6156f10cd32
Jeno Manivcsuk <manivchuk2002@gmail.com>
2022-12-15 15:02:02 +0000

    fix(ui): adds missing return

--- libs/ui/src/molecules/Greeting/Greeting.tsx
+++ libs/ui/src/molecules/Greeting/Greeting.tsx
@@ -12,12 +12,14 @@ export const Greeting: FC<GreetingProps> = ({ greeting, quote, isLoading }) => {
   }
 
   if (greeting && quote) {
-    <div>
-      <h1 className="pb-1 text-2xl font-semibold lg:text-3xl">{greeting}</h1>
-      <h3 className="text-sm lg:text-base text-zinc-700 max-w-[70ch] dark:text-zinc-400">
-        {quote}
-      </h3>
-    </div>;
+    return (
+      <div>
+        <h1 className="pb-1 text-2xl font-semibold lg:text-3xl">{greeting}</h1>
+        <h3 className="text-sm lg:text-base text-zinc-700 max-w-[70ch] dark:text-zinc-400">
+          {quote}
+        </h3>
+      </div>
+    );
   }
 
   return null;
