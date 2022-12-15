0fe82b371c8125142897459ec302d89976cfc113
Jeno Manivcsuk <manivchuk2002@gmail.com>
2022-12-15 15:36:10 +0000

    feat(ui): title uses loading skeleton

--- libs/ui/src/molecules/Greeting/Greeting.tsx
+++ libs/ui/src/molecules/Greeting/Greeting.tsx
@@ -8,7 +8,17 @@ type GreetingProps = {
 
 export const Greeting: FC<GreetingProps> = ({ greeting, quote, isLoading }) => {
   if (isLoading) {
-    return <p>Loading...</p>;
+    return (
+      <div className="w-60 h-24 border-2 rounded-md mx-auto mt-20">
+        <div className="flex animate-pulse flex-row items-center h-full justify-center space-x-5">
+          <div className="w-12 bg-gray-300 h-12 rounded-full " />
+          <div className="flex flex-col space-y-3">
+            <div className="w-36 bg-gray-300 h-6 rounded-md " />
+            <div className="w-24 bg-gray-300 h-6 rounded-md " />
+          </div>
+        </div>
+      </div>
+    );
   }
 
   if (greeting && quote) {
