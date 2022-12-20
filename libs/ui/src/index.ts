b746db03f370cec8bf6fb388166714acf470d76a
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-12-20 19:56:27 +0000

    feat(ui,client): adds 404 not found page

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,6 +1,7 @@
 export { Auth } from './pages/Auth';
+export { InternalError } from './pages/InternalError';
 export { Landing } from './pages/Landing';
+export { NotFound } from './pages/NotFound';
 export { TodaysActivity } from './pages/TodaysActivity';
 export { LoadingScreen } from './templates/LoadingScreen';
 export { Navbar } from './templates/Navbar';
-export { InternalError } from './pages/InternalError';
