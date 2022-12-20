9f4a0aaa389b17f321f2cd13ab36aab5942ed616
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2022-12-20 19:56:27 +0000

    feat(ui,client): adds 404 not found page

---
+++ apps/client/pages/404.tsx
@@ -0,0 +1,8 @@
+import { NotFound } from '@noodle/ui';
+import type { NextPage } from 'next';
+
+const ErrorPage: NextPage = () => {
+  return <NotFound />;
+};
+
+export default ErrorPage;
---
+++ libs/ui/src/pages/NotFound/index.ts
@@ -0,0 +1,1 @@
+export { NotFound } from './NotFound';
