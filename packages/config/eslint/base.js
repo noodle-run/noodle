42e0114f4f340dcc519946cc04b6055d00bad0e1
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2023-05-25 11:09:46 +0100

    feat: adds lint-staged on other files

--- packages/config/eslint/base.js
+++ packages/config/eslint/base.js
@@ -37,6 +37,7 @@ const config = {
       },
     ],
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'no-console': 'warn',
   },
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
