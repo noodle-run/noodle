402e34b96a166c5d088ae72b222bb5f3cfc02b9a
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2023-05-25 11:09:46 +0100

    feat: adds lint-staged on other files

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,6 +1,7 @@
 export default {
   '*.{js,jsx,ts,tsx}': [
     'eslint --max-warnings 0 --report-unused-disable-directives',
-    'prettier --check',
+    'prettier --list-different',
   ],
+  '*.{css,html,json,md,mdx,yaml,yml}': ['prettier --list-different'],
 };
