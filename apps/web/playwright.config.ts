ba4457cc484fb1133d0b6b77fc6d9ce04a34af96
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2023-06-08 00:44:36 +0100

    feat: adds playwright e2e testing to web app (#89)
    * feat: initialises playwright & eslint plugin
    
    * feat(ci): adds playwright to ci steps
    
    * fix(ci): playwright install after dep installation
    
    * fix(web): exclude e2e folder from vitest
    
    * fix(ci): install playwright browsers in web app dir
    
    * fix: root install playwright
    
    * fix: playwright install on agents
    
    * fix: ci agent step naming
    
    * chore: removes root playwright dep
    
    * chore: exclude files from vscode explorer
    
    * chore: adds recommended vscode extensions
    
    * chore: update lockfile

---
+++ apps/web/playwright.config.ts
@@ -0,0 +1,45 @@
+import { defineConfig, devices } from '@playwright/test';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '../../.env' });
+
+export default defineConfig({
+  testDir: './e2e',
+  fullyParallel: true,
+  forbidOnly: !!process.env['CI'],
+  retries: process.env['CI'] ? 2 : 1,
+  workers: process.env['CI'] ? 1 : '50%',
+  reporter: 'html',
+  use: {
+    baseURL: 'http://127.0.0.1:3000',
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+    {
+      name: 'Mobile Chrome',
+      use: { ...devices['Pixel 5'] },
+    },
+    {
+      name: 'Mobile Safari',
+      use: { ...devices['iPhone 12'] },
+    },
+  ],
+  webServer: {
+    command: 'pnpm dev',
+    cwd: '../../',
+    url: 'http://127.0.0.1:3000',
+    reuseExistingServer: !process.env['CI'],
+  },
+});
