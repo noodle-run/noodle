82208c46739a7e7c237f4aea11ce84887665ef7b
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2023-06-08 00:05:45 +0100

    fix(web): exclude e2e folder from vitest

--- apps/web/vitest.config.ts
+++ apps/web/vitest.config.ts
@@ -1,7 +1,10 @@
+import { defaultExclude } from 'vitest/config';
+
 import { mergeConfig, reactConfig } from '@noodle/vitest';
 
 export default mergeConfig(reactConfig, {
   test: {
     setupFiles: ['./vitest.setup.ts'],
+    exclude: [...defaultExclude, '**/e2e/**/*'],
   },
 });
