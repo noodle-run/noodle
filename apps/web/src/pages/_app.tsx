93f9ce53dd461eb3ad36c0c93f1b4e6dce364bdf
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2023-06-26 00:33:55 +0100

    feat: adds vercel analytics

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import { ThemeProvider } from 'next-themes';
 import { type AppProps } from 'next/app';
 
@@ -9,6 +10,7 @@ const App = ({ Component, pageProps }: AppProps) => {
   return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   );
 };
