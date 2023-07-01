57a49f8d540295a60ba6cfb05baf90b34df17903
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2023-07-01 12:41:52 +0100

    chore: removes eslint from lint-staged

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,6 +1,5 @@
 export default {
   '*.{js,jsx,ts,tsx}': [
-    'eslint --max-warnings 0 --report-unused-disable-directives',
     'prettier --list-different',
   ],
   '*.{css,html,json,md,mdx,yaml,yml}': ['prettier --list-different'],
