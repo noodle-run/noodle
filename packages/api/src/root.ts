be4bcfb492498b147b3d13bb650711166029c957
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2023-07-01 16:07:18 +0100

    feat: initialises prisma orm

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,8 +1,10 @@
 import { greetingRouter } from './routers/greeting';
+import { userRouter } from './routers/user';
 import { createRouter } from './setup/trpc';
 
 export const appRouter = createRouter({
   greeting: greetingRouter,
+  user: userRouter,
 });
 
 export type AppRouter = typeof appRouter;
