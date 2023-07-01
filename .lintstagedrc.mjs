0c5a62c456fe2f8a93f2b9e4544911f879bfb9a7
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2023-07-01 13:02:29 +0100

    feat: updates deps & installs next-seo (#121)
    * chore: updates pnpm version
    
    * chore: updates all deps
    
    * chore: removes eslint from lint-staged
    
    * feat: adds next-seo

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,7 +1,4 @@
 export default {
-  '*.{js,jsx,ts,tsx}': [
-    'eslint --max-warnings 0 --report-unused-disable-directives',
-    'prettier --list-different',
-  ],
+  '*.{js,jsx,ts,tsx}': ['prettier --list-different'],
   '*.{css,html,json,md,mdx,yaml,yml}': ['prettier --list-different'],
 };
