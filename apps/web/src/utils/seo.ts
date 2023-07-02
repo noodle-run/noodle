4b439caabb9553ef1179747c725a72f742d3ef99
Ahmed Elsakan <ahmedtarek3214@gmail.com>
2023-07-02 19:41:04 +0100

    feat(web): adds preview url card

--- apps/web/src/utils/seo.ts
+++ apps/web/src/utils/seo.ts
@@ -10,6 +10,18 @@ export const seo: DefaultSeoProps = {
     title: 'Noodle | Rethinking Student Productivity',
     description:
       'Noodle is a productivity app for students. It helps you manage your time, tasks, and goals.',
+    images: [
+      {
+        url: 'https://noodle.run/preview_card.png',
+        width: 1200,
+        height: 628,
+        alt: 'Noodle - Open source education platform',
+      },
+    ],
+    site_name: 'Noodle',
+  },
+  twitter: {
+    cardType: 'summary_large_image',
   },
   additionalLinkTags: [
     {
