b346e0fac1799d79b2edd4bc524558476bc3e8fd
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2023-07-02 17:26:03 +0100

    feat(web): adds favicons (#128)

--- apps/web/src/utils/seo.ts
+++ apps/web/src/utils/seo.ts
@@ -11,4 +11,27 @@ export const seo: DefaultSeoProps = {
     description:
       'Noodle is a productivity app for students. It helps you manage your time, tasks, and goals.',
   },
+  additionalLinkTags: [
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      href: '/apple-touch-icon.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      href: '/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      href: '/favicon-16x16.png',
+    },
+    {
+      rel: 'manifest',
+      href: '/site.webmanifest',
+    },
+  ],
 };
