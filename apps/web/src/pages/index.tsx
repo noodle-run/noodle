4bc9820499860c8154751b46f3da3cad43919acb
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2023-07-02 17:32:15 +0100

    feat: quick something better for home page

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,34 +1,18 @@
 import { type NextPage } from 'next';
-import { signIn, signOut, useSession } from 'next-auth/react';
-
-import { api } from '@/utils/api';
 
 const Home: NextPage = () => {
-  const { data: session, status } = useSession();
-  const { data } = api.greeting.hello.useQuery({});
-
-  if (status === 'loading') {
-    return <div>Loading...</div>;
-  }
-
-  if (!session?.user) {
-    return (
-      <main>
-        <h1>Noodle - Open Source Education Platform</h1>
-        <p>{data?.message}</p>
-        <button type="button" onClick={() => void signIn('github')}>
-          Sign in with Github
-        </button>
-      </main>
-    );
-  }
-
   return (
-    <main>
-      <h1>Hello there, {session.user.name}</h1>
-      <button type="button" onClick={() => void signOut()}>
-        Sign out
-      </button>
+    <main className="container mx-auto flex min-h-screen flex-col items-center justify-center">
+      <h1 className="scroll-m-20 pb-4 text-center text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Noodle
+      </h1>
+      <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Rethinking Student Productivity
+      </h1>
+      <p className="text-gray-11 dark:text-graydark-11 text-center leading-7 [&:not(:first-child)]:mt-6">
+        Cooking is being undertaken by the Noodle team. <br /> Please wait
+        warmly until it is ready.
+      </p>
     </main>
   );
 };
