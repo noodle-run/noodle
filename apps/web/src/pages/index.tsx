ab632b9520c81ae747210537a8c84acc75e41c7b
Alexander Ament <118925546+Agent-AA@users.noreply.github.com>
2023-07-07 15:03:45 +0000

    Revise title and phrasing

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -4,7 +4,7 @@ import { type NextPage } from 'next';
 const Home: NextPage = () => {
   return (
     <main className="container mx-auto flex h-screen flex-col items-center justify-center">
-      <h1 className="scroll-m-20 pb-2 text-center text-5xl font-extrabold tracking-tight lg:pb-4 lg:text-6xl">
+      <h1 className="scroll-m-20 pb-2 text-center text-5xl font-extrabold tracking-tight lg:pb-4 lg:text-7xl text-primary-500">
         Noodle
       </h1>
       <h1 className="scroll-m-20 text-center text-5xl font-extrabold tracking-tight lg:text-6xl">
@@ -12,8 +12,7 @@ const Home: NextPage = () => {
       </h1>
       <p className="text-gray-11 dark:text-graydark-11 text-center text-base leading-7 lg:text-lg [&:not(:first-child)]:mt-6">
         <Balancer>
-          Cooking is being undertaken by the Noodle team. Please wait warmly
-          until it is ready.
+          The Noodle Team is currently cooking up something awesome. Stay tuned!
         </Balancer>
       </p>
       <p className="text-gray-11 dark:text-graydark-11 text-center text-base leading-7 lg:text-lg [&:not(:first-child)]:mt-6">
