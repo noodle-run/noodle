e08eaf84ab11128c59d05e0d615fded885f440d4
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2023-07-09 13:49:12 +0100

    feat(web): rate limit the api

---
+++ apps/web/src/middleware.ts
@@ -0,0 +1,28 @@
+import type { NextRequest } from 'next/server';
+import { Ratelimit } from '@upstash/ratelimit';
+import { Redis } from '@upstash/redis';
+import { NextResponse } from 'next/server';
+
+const redis = new Redis({
+  url: 'https://eternal-deer-42544.upstash.io',
+  token: process.env['UPSTASH_TOKEN']!,
+});
+
+const ratelimit = new Ratelimit({
+  redis: redis,
+  limiter: Ratelimit.slidingWindow(5, '10 s'),
+});
+
+export default async function middleware(
+  request: NextRequest,
+): Promise<Response | undefined> {
+  const ip = request.ip ?? '127.0.0.1';
+  const { success } = await ratelimit.limit(ip);
+  return success
+    ? NextResponse.next()
+    : NextResponse.redirect(new URL('/blocked', request.url));
+}
+
+export const config = {
+  matcher: '/api/:path*',
+};
