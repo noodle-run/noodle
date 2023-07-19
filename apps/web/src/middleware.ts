56338da383741f0e4e13a1a8ccb7a85346157947
Ahmed Elsakan <ahmedtarek3214@gmail.com>
2023-07-19 20:06:40 +0100

    i'm a dumbass

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -6,8 +6,8 @@ import { NextResponse } from 'next/server';
 import { env } from '@noodle/env';
 
 const redis = new Redis({
-  url: env.REDIS_URL,
-  token: env.REDIS_TOKEN,
+  url: env.UPSTASH_REDIS_REST_URL,
+  token: env.UPSTASH_REDIS_REST_TOKEN,
 });
 
 const ratelimit = new Ratelimit({
