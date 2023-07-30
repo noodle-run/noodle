f1f214e8561ab5f3dfbce1e6343433979978ee8f
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2023-07-30 20:25:48 +0100

    feat(api,db): adds feedback route

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,8 +1,10 @@
+import { feedbackRouter } from './routers/feedback';
 import { waitListRouter } from './routers/wait-list';
 import { createRouter } from './setup/trpc';
 
 export const appRouter = createRouter({
   waitlist: waitListRouter,
+  feedback: feedbackRouter,
 });
 
 export type AppRouter = typeof appRouter;
