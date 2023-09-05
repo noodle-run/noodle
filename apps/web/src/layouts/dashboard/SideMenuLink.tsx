8ce7402ce39b494da105c4575c9274375be3cdeb
Gedeon <hello@gedeondoescode.com>
2023-09-04 20:16:15 -0400

    feat: implement side menu toggling (#312)
    * feat: implement side menu toggling
    
    * feat: some minor improvements
    
    ---------
    
    Co-authored-by: Ahmed Elsakaan <ahmedtarek3214@gmail.com>

--- apps/web/src/layouts/dashboard/SideMenuLink.tsx
+++ apps/web/src/layouts/dashboard/SideMenuLink.tsx
@@ -1,5 +1,6 @@
 import { type FC } from 'react';
 import type { IconNames } from '@/components/Icon';
+import { motion } from 'framer-motion';
 
 import { Button } from '@noodle/ui';
 
@@ -10,9 +11,15 @@ type SideMenuLinkProps = {
   href: string;
   icon: IconNames;
   label: string;
+  isSideMenuExpanded: boolean;
 };
 
-export const SideMenuLink: FC<SideMenuLinkProps> = ({ href, icon, label }) => {
+export const SideMenuLink: FC<SideMenuLinkProps> = ({
+  href,
+  icon,
+  label,
+  isSideMenuExpanded = true,
+}) => {
   return (
     <li>
       <Button asChild variant="muted" className="w-full">
@@ -20,8 +27,15 @@ export const SideMenuLink: FC<SideMenuLinkProps> = ({ href, icon, label }) => {
           href={href}
           activeClassName="text-gray-12 dark:text-graydark-12"
         >
-          <Icon name={icon} />
-          <span>{label}</span>
+          <Icon name={icon} className="min-w-max" />
+          <motion.span
+            animate={{
+              opacity: isSideMenuExpanded ? 1 : 0,
+            }}
+            transition={{ duration: 0.2 }}
+          >
+            {label}
+          </motion.span>
         </ActiveLink>
       </Button>
     </li>
