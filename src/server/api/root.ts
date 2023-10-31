4cac8d2b3e877d7c14ee6211b6031e2016ccde37
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2023-10-31 23:50:04 +0000

    feat: weather, notebooks table and node 20
    This commit brings the following changes:
    
    - introduces the side box with weather data
    - introduces the notebooks table in home dashboard layout
    - upgrades nodejs to v20
    - upgrades pnpm to 8.10.0

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,9 +1,11 @@
 import { feedbackRouter } from "./routers/feedback";
 import { moduleRouter } from "./routers/module";
 import { waitlistRouter } from "./routers/waitlist";
+import { weatherRouter } from "./routers/weather";
 import { createTRPCRouter } from "./trpc";
 
 export const appRouter = createTRPCRouter({
+  weather: weatherRouter,
   waitlist: waitlistRouter,
   feedback: feedbackRouter,
   module: moduleRouter,
