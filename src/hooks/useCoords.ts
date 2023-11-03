a04a93b14e2aea62f377816eb78e1cf81bc23ebf
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2023-11-03 19:45:30 +0000

    fix formatting

--- src/hooks/useCoords.ts
+++ src/hooks/useCoords.ts
@@ -1,6 +1,6 @@
-'use client';
+"use client";
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 export const useCoords = () => {
   const [coords, setCoords] = useState<{
