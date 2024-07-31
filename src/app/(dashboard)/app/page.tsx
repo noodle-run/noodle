f66b643d460480a98fa67c83d76373dd8adc9dc8
Kyle Dickey <k@kyle.so>
2024-07-31 04:08:15 -0600

    feat: add welcome name

--- src/app/(dashboard)/app/page.tsx
+++ src/app/(dashboard)/app/page.tsx
@@ -1,9 +1,23 @@
+'use client';
+
+import { useUser } from '@clerk/nextjs';
+
 export default function DashboardHome() {
+  const { user } = useUser();
+
   return (
     <div className="flex flex-1">
       <div className="flex-1">
         <div className="space-y-3">
-          <h1 className="text-4xl font-semibold">Good afternoon, Ahmed!</h1>
+          <h1 className="text-4xl font-semibold">
+            Good afternoon,{' '}
+            {user?.firstName
+              ? user.firstName
+              : user?.emailAddresses[0]?.emailAddress
+                  .split('@')[0]
+                  ?.split('.')[0]}
+            !
+          </h1>
           <p className="max-w-prose text-balance text-sm leading-6 text-foreground-muted">
             “The final wisdom of life requires not the annulment of incongruity
             but the achievement of serenity within and above it.” - Reinhold
