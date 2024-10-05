1fecc37515b2772e0be7d87d5bf2d549b0be8e08
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2024-10-05 11:19:59 +0100

    chore: updates deps

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 import { fileURLToPath } from 'node:url';
 
-import createJiti from 'jiti';
+import { createJiti } from 'jiti';
 
 const jiti = createJiti(fileURLToPath(import.meta.url));
 
-jiti('./src/env');
+await jiti.import('./src/env');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
